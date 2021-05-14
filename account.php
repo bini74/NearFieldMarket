1927f0d6b59a73f9a961b500957123a7996650e0
julien bonzi <69221560+bini74@users.noreply.github.com>
2021-05-14 13:36:38 +0200

    dashboard done

--- account.php
+++ account.php
@@ -7,6 +7,7 @@
 
             <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <link rel="stylesheet" href="css/account.css" />
             <title>Connexion</title>
         </head>
         <body>
@@ -49,7 +50,7 @@
             <form action="connexion.php" method="post">
                 <h2 class="text-center">Connexion</h2>       
                 <div class="form-group">
-                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off" >
                 </div>
                 <div class="form-group">
                     <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
@@ -60,28 +61,5 @@
             </form>
             <p class="text-center"><a href="/inscription.php">Inscription</a></p>
         </div>
-        <style>
-            .login-form {
-                width: 340px;
-                margin: 50px auto;
-            }
-            .login-form form {
-                margin-bottom: 15px;
-                background: #f7f7f7;
-                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-                padding: 30px;
-            }
-            .login-form h2 {
-                margin: 0 0 15px;
-            }
-            .form-control, .btn {
-                min-height: 38px;
-                border-radius: 2px;
-            }
-            .btn {        
-                font-size: 15px;
-                font-weight: bold;
-            }
-        </style>
         </body>
 </html>
